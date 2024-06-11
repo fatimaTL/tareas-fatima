e98e5eef5e724b2f3d36806b96f7076983e3a6ad
fatmaTL <2023-40062@liceocanadiense.edu.gt>
2024-06-10 18:15:13 -0600

    tarea 1

---
+++ ejemplo 1.py
@@ -0,0 +1,13 @@
+a=16
+b=3
+c=5
+d=18
+e=45
+resultado=a+b+c+d+e
+resultado1=a-b-c-d-e
+resultado2=a*b*c*d*e
+resultado3=a/b/c/d/e
+print("la suma es de: "+str(resultado))
+print("la resta es de: "+str(resultado1))
+print("la multiplicacion es de: "+str(resultado2))
+print("la division es de: "+str(resultado3))
---
+++ ejemplo2.py
@@ -0,0 +1,13 @@
+n1=int (input ("ingrese su primer dato"))
+n2=int (input ("ingrese su segundo dato"))
+n3=int (input ("ingrese su tercer dato"))
+n4=int (input ("ingrese su cuarto dato"))
+n5=int (input ("ingrese su quinto dato"))
+resultado=n1+n2+n3+n4+n5
+resultado1=n1-n2-n3-n4-n5
+resultado2=n1*n2*n3*n4*n5
+resultado3=n1/n2/n3/n4/n5
+print("la suma es de: "+str(resultado))
+print("la resta es de: "+str(resultado1))
+print("la multiplicacion es de: "+str(resultado2))
+print("la division es de: "+str(resultado3))
